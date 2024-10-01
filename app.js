eb5d05a9c151e0898fe281ef4afdf1a1f40fd22e
omiras <oscar.miras@gmail.com>
2024-10-01 17:02:20 +0200

    done

--- app.js
+++ app.js
@@ -5,20 +5,20 @@ document.querySelectorAll('.emotion').forEach(img => {
 
             // TODO: COMPLETAR: Necesitamos hacer un fetch al endpoint correcto, procesar los datos y asignar el libro recuperado a la variable 'book'
 
-            const book = {};
+            const book = {}; // TODO: Aquí deberíamos tener un objeto con información del libro de la REST API
 
             // NO MODIFICAR A PARTIR DE AQUÍ
-            document.getElementById('book-title').textContent = book.title;
-            document.getElementById('book-isbn').textContent = book.isbn;
-            document.getElementById('book-price').textContent = book.price.toFixed(2);
-            document.getElementById('book-description').textContent = book.description;
-            document.getElementById('book-image').src = book.imageURL;
+            document.querySelector('#book-title').textContent = book.title;
+            document.querySelector('#book-isbn').textContent = book.isbn;
+            document.querySelector('#book-price').textContent = book.price;
+            document.querySelector('#book-description').textContent = book.description;
+            document.querySelector('#book-image').src = book.imageURL;
 
-            document.getElementById('book-container').style.display = 'block';
+            document.querySelector('#book-container').style.display = 'block';
 
         } catch (error) {
             console.error('Error fetching book:', error.message);
-            alert('An error ocurred. Use the developer tools!');
+            alert('An error ocurred. Use the developers tools!');
         }
     });
 });
